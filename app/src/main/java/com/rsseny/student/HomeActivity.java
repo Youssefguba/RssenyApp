afa236ce62a78d1c18bb73328e1d16ca056b0e78
Youssefguba <youssefguba6@gmail.com>
2019-06-22 00:33:34 +0200

    Initial Commit

--- app/src/main/java/com/rsseny/student/HomeActivity.java
+++ app/src/main/java/com/rsseny/student/HomeActivity.java
@@ -1,16 +1,41 @@
 package com.rsseny.student;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ornach.nobobutton.NoboButton;
+
 public class HomeActivity extends AppCompatActivity {
+
+    NoboButton knowMorebtn, consultationbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        knowMorebtn = findViewById(R.id.faculties_button);
+        consultationbtn = findViewById(R.id.consultation_button);
+
+
+        knowMorebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent knowMoreIntent = new Intent();
+                startActivity(knowMoreIntent);
+            }
+        });
+
+        consultationbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent consultationIntent = new Intent();
+                startActivity(consultationIntent);
+            }
+        });
 
     }
 }
